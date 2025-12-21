44268c5c18a1c85fa1266ba27d947961b784ee17
Haripriya S <sharipriya008@gmail.com>
2025-12-21 15:46:29 +0530

    Update App.js

--- netgear-login/src/pages/Login/App.js
+++ netgear-login/src/pages/Login/App.js
@@ -51,7 +51,7 @@ export default function App() {
     }
 
     try {
-      const res = await fetch("http://localhost:5000/api/login", {
+      const res = await fetch("https://routing-13jd.onrender.com/api/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username: username.trim(), password }),
